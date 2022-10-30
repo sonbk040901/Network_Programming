dc8bafae15da7052d7f0b8b1d94cbfa613577f55
Sonle <leducson007@gmail.com>
2022-10-30 12:37:53 +0700

    w2

---
+++ w1/include/user.c
@@ -0,0 +1,138 @@
+#include "user.h"
+
+User newUser(char *username, char *password, Status status)
+{
+    User user = malloc(sizeof(struct user));
+    user->username = strdup(username);
+    user->password = strdup(password);
+    user->status = status;
+    return user;
+}
+void blockUser(User user)
+{
+    user->status = blocked;
+}
+void activeUser(User user)
+{
+    user->status = active;
+}
+void setPassword(User user, char *password)
+{
+    free(user->password);
+    user->password = strdup(password);
+}
+Status getUserStatus(User user)
+{
+    return user->status;
+}
+void freeUser(User user)
+{
+    free(user->username);
+    free(user->password);
+    free(user);
+}
+
+char *getUserName(User user)
+{
+    return user->username;
+}
+char *getUserPassword(User user)
+{
+    return user->password;
+}
+Dllist makUsersList(char *filename)
+{
+    Dllist list = new_dllist();
+    IS is = new_inputstruct(filename);
+    if (is == NULL)
+    {
+        printf("khong mo duoc file \n");
+        exit(0);
+    }
+    while (get_line(is) >= 0)
+    {
+        dll_append(list, new_jval_v(newUser(is->fields[0], is->fields[1], atoi(is->fields[2]))));
+    }
+    jettison_inputstruct(is);
+    return list;
+}
+User existsUser(Dllist list, char *username)
+{
+    Dllist root;
+    User user;
+    dll_traverse(root, list)
+    {
+        user = (User)jval_v(dll_val(root));
+        if (strcmp(user->username, username) == 0)
+        {
+            return user;
+        }
+    }
+    return (User)0;
+}
+void addUser(Dllist list, User user)
+{
+    dll_append(list, new_jval_v(user));
+}
+void export(Dllist list, char *filename)
+{
+    Dllist temp;
+    FILE *fout = fopen(filename, "w+");
+    dll_traverse(temp, list)
+    {
+        User user = (User)jval_v(dll_val(temp));
+        // printf("User: %s %s %d\n", user->username, user->password, user->status);
+        fprintf(fout, "%s %s %d", user->username, user->password, user->status);
+        if (temp != dll_last(list))
+        {
+            fprintf(fout, "\n");
+        }
+    }
+    fclose(fout);
+}
+void freeUserList(Dllist list)
+{
+    Dllist temp;
+    dll_traverse(temp, list)
+    {
+        User user = (User)jval_v(dll_val(temp));
+        freeUser(user);
+    }
+}
+int activate(User user, char *activationCode)
+{
+    if (strcmp(activationCode, ACTIVATION_CODE) != 0)
+    {
+        return 0;
+    }
+    else
+    {
+        activeUser(user);
+        return 1;
+    }
+}
+User verify(Dllist list, char *username, char *password)
+{
+    Dllist root;
+    User user;
+    dll_traverse(root, list)
+    {
+        user = (User)jval_v(dll_val(root));
+        if (strcmp(username, user->username) == 0)
+        {
+            if (strcmp(password, user->password) == 0)
+            {
+                return user;
+            }
+            else
+            {
+                return (User)0;
+            }
+        }
+    }
+    return (User)0;
+}
+int checkPassword(User user, char *password)
+{
+    return strcmp(password, user->password) == 0;
+}
---
+++ w2/src/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <ctype.h>
+#include <string.h>
+struct hostent *gethost(char *str, int mode);
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3 || strlen(argv[1]) != 1 || !isdigit(argv[1][0]) || (atoi(argv[1]) != 1 && atoi(argv[1]) != 2))
+	{
+		puts("Wrong parameter");
+		exit(1);
+	}
+	struct hostent *he;
+	struct in_addr **addr_list;
+	int type = atoi(argv[1]);
+	he = gethost(argv[2], type);
+	if (he == (void *)1)
+	{
+		puts("Wrong parameter");
+		exit(1);
+	}
+	if (he == NULL)
+	{
+		puts("Not found information");
+		exit(1);
+	}
+	if (type == 2)
+	{
+		printf("Official IP: %s\n", inet_ntoa(*(struct in_addr *)he->h_addr));
+		printf("Alias IP:\n");
+		addr_list = (struct in_addr **)he->h_addr_list;
+		for (int i = 1; addr_list[i] != NULL; i++)
+		{
+			printf("%s\n", inet_ntoa(*addr_list[i]));
+		}
+	}
+	else
+	{
+		printf("Official name: %s\n", he->h_name);
+		printf("Alias name:\n");
+		for (int i = 0; he->h_aliases[i] != NULL; i++)
+		{
+			printf(" %s\n", he->h_aliases[i]);
+		}
+	}
+	return 0;
+}
+struct hostent *gethost(char *str, int mode)
+{
+	struct in_addr addr;
+	int check = inet_aton(str, &addr);
+	if ((check && mode == 2) || (!check && mode == 1))
+	{
+		return (void *)1;
+	}
+	if (mode == 2)
+	{
+		return gethostbyname(str);
+	}
+	else
+	{
+		return gethostbyaddr(&addr, sizeof(addr), AF_INET);
+	}
+}
